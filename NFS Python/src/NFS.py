00e50b18ba741fe06800787f688d18e9db2440f9
Basile Jezequel <119074581+basilegithub@users.noreply.github.com>
2025-08-11 19:09:56 +0200

    Add files via upload

---
+++ NFS Python/src/NFS.py
@@ -0,0 +1,218 @@
+# This is the file that execute the Quadratic Sieve algorithm
+
+## import libraries
+
+ZQDFEGSRHTDR = 35279032783773322923326661023
+
+import math, time, os
+from datetime import datetime
+import log
+import parse_config
+from utils import *
+from polynomial_functions import *
+from generate_primes import *
+import mono_cpu_polynomial_selection
+import multi_cpu_polynomial_selection
+import mono_cpu_sieve
+import multi_cpu_sieve
+from relations import *
+from build_matrix import *
+from gaussian_elimination import *
+from wiedemann import *
+from block_lanczos import *
+import compute_solutions
+
+## Set path to find config file
+
+CONFIG_PATH = os.path.abspath("../config/config.ini")
+    
+## NFS functions
+    
+def initialize(n):
+    
+    d = int(pow(3*math.log(n)/math.log(math.log(n)),1/3))
+    first_B = int(math.exp(pow((8/9)*math.log(n),1/3)*pow(math.log(math.log(n)),2/3)))
+    B = 10*first_B//70
+    
+    return d, B
+    
+def initialize_2(f_x, n, m1, d, primes, leading_coeff):
+    pairs_used,R_p,logs,tmp = [],[],[],10*d
+    divide_leading,pow_div = [],[]
+    for p in primes:
+        if not leading_coeff%p:
+            divide_leading.append(p)
+            u = p
+            while not leading_coeff%u: u *= p
+            pow_div.append(u//p)
+        logs.append(round(math.log2(p)))
+        if p > tmp: R_p.append(find_roots_poly(f_x,p))
+        else: R_p.append(fast_roots(f_x,p))
+        if len(R_p[-1]) == d:
+            if d&1: pairs_used.append([[leading_coeff*p],leading_coeff*pow(p,d),[[1,1]],p*m1,[1],[0,p],1])
+            else: pairs_used.append([[leading_coeff*p],leading_coeff*p,[[1,1]],p*m1,[1],[0,p],1])
+    for p in divide_leading:
+        for i in range(len(pairs_used)): pairs_used[i].append(True)
+        
+    B_prime = sum([len(i) for i in R_p]) + 1
+    
+    return pairs_used, R_p, logs, divide_leading, pow_div, B_prime
+    
+def initialize_3(n, f_x, f_prime, const1, leading_coeff):
+    k = 3*n.bit_length()
+    Q = []
+    q = const1+1
+    if not q%2: q += 1
+    while len(Q) < k:
+        if is_prime(q) and leading_coeff%q:
+            if not n%q: return q, n//q
+            else:
+                tmp = find_roots_poly(f_x,q)
+                for r in tmp:
+                    if eval_mod(f_prime,r,q): Q.append([q,r])
+        q += 2
+        
+    return Q, k
+    
+def NFS(n):
+    now = datetime.now()
+    
+    LOG_PATH = os.path.abspath("../logs/log_"+str(now.year)+str(now.month)+str(now.day)+"_"+str(now.hour)+str(now.minute)+str(now.second)+".txt")
+    
+    parameters = parse_config.parse_config(CONFIG_PATH)
+    
+    flag_use_batch_smooth_test = parameters[0].lower() in ["true"]
+    flag_gaussian_pivot = parameters[1].lower() in ["true"]
+    flag_lanczos = parameters[2].lower() in ["true"]
+    flag_square_root_couveignes = parameters[3].lower() in ["true"]
+    const = int(parameters[4])
+    BLOCK_SIZE = int(parameters[5])
+    nb_poly_coarse_eval = int(parameters[6])
+    nb_poly_precise_eval = int(parameters[7])
+    prime_bound = int(parameters[8])
+    nb_roots = int(parameters[9])
+    multiplier = int(parameters[10])
+    NB_CPU_POLY_SELECTION = int(parameters[11])
+    NB_CPU_SIEVE = int(parameters[12])
+    
+    n = int(n)
+    
+    d, B = initialize(n)
+    
+    primes = create_smooth_primes_base(B)
+    for p in primes:
+        if not n%p: return p,n//p
+
+    prod_primes = math.prod(primes)
+        
+    const1, const2 = const*primes[-1], const*primes[-1]*primes[-1]
+    
+    if NB_CPU_POLY_SELECTION == 1:
+        f_x,m0,m1,tmp,_ = mono_cpu_polynomial_selection.poly_search(n,primes,nb_roots,prime_bound,multiplier,
+                                                                 int(pow(n,1/(d+1))),d,nb_poly_coarse_eval,
+                                                                 nb_poly_precise_eval,LOG_PATH)
+        
+    else:
+        f_x,m0,m1,tmp,_ = multi_cpu_polynomial_selection.poly_search(n,primes,nb_roots,prime_bound,multiplier,
+                                                                 int(pow(n,1/(d+1))),d,nb_poly_coarse_eval,
+                                                                 nb_poly_precise_eval,NB_CPU_POLY_SELECTION,LOG_PATH)
+
+    log.write_log(LOG_PATH, "poly search completed, parameters : m0 = "+str(m0)+" ; m1 = "+str(m1)+" ; d = "+str(d)+"\n")
+    
+    f_x, m0, M = mono_cpu_polynomial_selection.evaluate_polynomial_quality(f_x,B,m0,m1,primes,LOG_PATH)
+    
+    leading_coeff = f_x[0]
+    zeros_f = get_complex_roots(f_x)
+    zeros = [leading_coeff*i for i in zeros_f]
+    f_prime = get_derivative(f_x)
+    
+    g = [1,f_x[1]]
+    for i in range(2,len(f_x)): g.append(f_x[i]*pow(leading_coeff,i-1))
+    g_prime = get_derivative(g)
+
+    g_prime_sq,g_prime_eval = div_poly(poly_prod(g_prime,g_prime),g),pow(leading_coeff, d-2, n)*eval_F(m0,m1,f_prime,d-1)%n
+    
+    delta = []
+    for r in range(d):
+        tmp = []
+        for i in range(d):
+            delt = 0
+            for j in range(d-i):
+                if zeros[r].real == 0 or zeros[r].imag == 0 or j == 0: delt += (abs(g[-i-j-2])*pow(leading_coeff,j)*pow(my_norm(zeros_f[r]),j))
+                else: delt += (abs(g[-i-j-2])*pow(leading_coeff,j)*pow(my_norm(zeros_f[r]),j))+1
+            tmp.append(delt)
+        delta.append(tmp)
+
+    
+    inert_set = []
+    for p in primes:
+        if m1%p and leading_coeff%p and irreducibility(g,p): inert_set.append(p)
+    
+    pairs_used, R_p, logs, divide_leading, pow_div, B_prime = initialize_2(f_x, n, m1, d, primes, leading_coeff)
+        
+    Q, k = initialize_3(n, f_x, f_prime, B, leading_coeff)
+        
+    log.write_log(LOG_PATH, "components of factor base : ("+str(len(primes))+","+str(B_prime)+","+str(k)+","+str(len(divide_leading))+")")
+    log.write_log(LOG_PATH, "starting with "+str(len(pairs_used))+" free relations")
+    
+    if NB_CPU_SIEVE <= 1:
+        if NB_CPU_SIEVE < 1: print("NB_CPU parameter incorrectly set. Must be > 0. Sieving with 1 CPU.")
+        pairs_used, V = mono_cpu_sieve.find_relations(f_x,leading_coeff,g,primes,R_p,Q,B_prime,divide_leading,
+                                                      prod_primes,pow_div,pairs_used,const1,const2,logs,m0,m1,
+                                                      M,d,n,flag_use_batch_smooth_test,LOG_PATH)
+
+    else:
+        pairs_used, V = multi_cpu_sieve.find_relations(f_x,leading_coeff,g,primes,R_p,Q,B_prime,divide_leading,
+                                                       prod_primes,pow_div,pairs_used,const1,const2,logs,m0,m1,
+                                                       M,d,n,flag_use_batch_smooth_test,LOG_PATH,NB_CPU_SIEVE)
+
+    print("")
+    log.write_log(LOG_PATH, "sieving complete, building matrix...")
+    if flag_gaussian_pivot:
+        matrix = build_dense_matrix(pairs_used, primes, R_p, Q, divide_leading)
+        length = len(matrix[0])
+        matrix, N, U = siqs_build_matrix_opt(matrix)
+    else:
+        matrix = build_sparse_matrix(pairs_used,primes,R_p,Q,divide_leading)
+        matrix = transpose_sparse(matrix, V)
+    log.write_log(LOG_PATH, "matrix built "+str(len(pairs_used))+"x"+str(V)+" reducing...")
+    #if not flag_gaussian_pivot:
+    #    matrix,pairs_used = reduce_sparse_matrix(matrix,pairs_used)
+    log.write_log(LOG_PATH, "matrix built "+str(len(pairs_used))+"x"+str(V)+" finding kernel...")
+
+    time_1 = datetime.now()
+
+    if flag_gaussian_pivot:
+        null_space = siqs_solve_matrix_opt(matrix, N, U)
+        log.write_log(LOG_PATH, str(len(null_space))+" kernel vectors found")
+        for vec in null_space:
+            vec = compute_solutions.convert_to_binary(vec, length)
+            flag, res = compute_solutions.compute_factors(pairs_used,vec,n,primes,f_x,g,g_prime,g_prime_sq,g_prime_eval,
+                                                        m0,m1,leading_coeff,d,inert_set,zeros,delta,M,flag_square_root_couveignes,
+                                                        time_1,LOG_PATH)
+            if flag: return res
+
+    else:
+        if flag_lanczos:
+            while True:
+                null_space = block_lanczos(matrix,len(pairs_used),BLOCK_SIZE, LOG_PATH)
+                
+                log.write_log(LOG_PATH, str(len(null_space))+" kernel vectors found")
+                for vec in null_space:
+                    vec = compute_solutions.convert_to_binary_lanczos(vec, len(pairs_used))
+                    flag, res = compute_solutions.compute_factors(pairs_used,vec,n,primes,f_x,g,g_prime,g_prime_sq,g_prime_eval,
+                                                             m0,m1,leading_coeff,d,inert_set,zeros,delta,M,flag_square_root_couveignes,
+                                                             time_1,LOG_PATH)
+                    if flag: return res
+
+        else:
+            mini_poly_estim = 1
+            while True:
+                null_space, mini_poly_estim = wiedemann(matrix,len(pairs_used),BLOCK_SIZE,mini_poly_estim)
+                null_space = reduce_null_space_vectors(null_space)
+                log.write_log(LOG_PATH, str(len(null_space))+" kernel vectors found")
+                for vec in null_space:
+                    flag, res = compute_solutions.compute_factors(pairs_used,vec,n,primes,f_x,g,g_prime,g_prime_sq,g_prime_eval,
+                                                             m0,m1,leading_coeff,d,inert_set,zeros,delta,M,flag_square_root_couveignes,
+                                                             time_1,LOG_PATH)
+                    if flag: return res
---
+++ NFS Python/src/build_matrix.py
@@ -0,0 +1,247 @@
+# This is the file to build the binary matrix
+
+from utils import *
+from polynomial_functions import *
+
+# Create the sparse matrix:
+# Consider the dense binary matrix A[i][j]
+# The sparse matrix M is defined by M[i] contains j if and only if A[i][j] = 1
+# For large numbers to factor, this reduces that memory requirement for the linear algebra part
+# Each prime in the factor base corresponds to one line
+# Each relation corresponds to one column
+def build_sparse_matrix(pairs,rat,alg,qua,div_lead):
+    matrix = []
+    for r in pairs:
+        index = 1
+        
+        if r[3] < 0: line = [0]
+        else: line = []
+        for p in rat:
+            tmp = p
+            tmp2 = 0
+            while not r[3]%tmp:
+                tmp *= p
+                tmp2 ^= 1
+            if tmp2: line.append(index)
+            index += 1
+            
+        if r[1] < 0: line.append(index)
+        index += 1
+        for p in range(len(alg)):
+            tmp = rat[p]
+            tmp2 = 0
+            while not r[1]%tmp:
+                tmp *= rat[p]
+                tmp2 ^= 1
+            for i in range(len(alg[p])):
+                if not eval_mod(r[5],alg[p][i],rat[p]):
+                    if tmp2: line.append(index)
+                index += 1
+                
+        for p in range(len(qua)):
+            if compute_legendre_character(eval_mod(r[5],qua[p][1],qua[p][0]),qua[p][0]) == -1: line.append(index)
+            index += 1
+            
+        for p in range(len(div_lead)):
+            if r[7+p]:
+                tmp = div_lead[p]
+                tmp2 = 0
+                while not r[1]%tmp:
+                    tmp *= div_lead[p]
+                    tmp2 ^= 1
+                if tmp2: line.append(index)
+            index += 1
+            
+        if r[6]&1: line.append(index)
+        matrix.append(line)
+    return matrix
+    
+# Reduce the sparse matrix, according to various rules:
+# 1. If a line contains only one non-zero value, then it is deleted as well as the column containing the corresponding non-zero value
+# 2. If a line contrains no non-zero value, then it is deleted
+# 3. We only keep 10 more columns than lines, to ensure we still have solutions while reducing the matrix size
+def reduce_sparse_matrix(matrix,pairs):
+    need = True
+    while need:
+        need = False
+        i = 0
+        while i < len(matrix):
+            if len(matrix[i]) == 1:
+                need = True
+                coeff = matrix[i][0]
+                for j in range(len(matrix)):
+                    if j != i:
+                        stored = -1
+                        for z in range(len(matrix[j])):
+                            if matrix[j][z] == coeff: stored = z
+                            elif matrix[j][z] > coeff: matrix[j][z] -= 1
+                        if stored > -1: del matrix[j][stored]
+                del pairs[coeff]
+                del matrix[i]
+            else: i += 1
+        i = 0
+        while i < len(matrix):
+            if matrix[i] == []:
+                del matrix[i]
+                need = True
+            else: i += 1
+        length = len(matrix)+10
+        for i in range(len(matrix)):
+            if matrix[i][0] >= length:
+                need = True
+                matrix[i] = []
+            else:
+                for j in range(len(matrix[i])):
+                    if matrix[i][-j-1] < length:
+                        if j > 0:
+                            need = True
+                            matrix[i] = matrix[i][:len(matrix[i])-j]
+                        break
+    return matrix, pairs
+    
+def reduce_matrix(matrix,pairs):
+    weights = [len(i) for i in matrix]
+    sorted = [0]
+    for i in range(1,len(weights)):
+        a = 0
+        b = len(sorted)-1
+        tmp = (a+b)>>1
+        while a <= b and weights[sorted[tmp]] != weights[i]:
+            if weights[sorted[tmp]] < weights[i]: a = tmp+1
+            else: b = tmp-1
+            tmp = (a+b)>>1
+        if weights[sorted[tmp]] == weights[i]: sorted.insert(tmp,i)
+        else: sorted.insert(a,i)
+
+    need = True
+    while need:
+        need = (weights[sorted[0]] < 2)
+        while weights[sorted[0]] < 2:
+            tmp = sorted[0]
+            if weights[tmp] == 0:
+                del matrix[tmp]
+                del weights[tmp]
+            elif weights[tmp] == 1:
+                coeff = matrix[tmp][0]
+                del pairs[coeff]
+                for j in range(len(matrix)):
+                    stored = -1
+                    for z in range(weights[j]):
+                        if matrix[j][z] == coeff: stored = z
+                        elif matrix[j][z] > coeff: matrix[j][z] -= 1
+                    if stored > -1:
+                        del matrix[j][stored]
+                        weights[j] -= 1
+                        lol = sorted.index(j)
+                        while lol > 0 and weights[sorted[lol]] < weights[sorted[lol-1]]:
+                            sorted[lol],sorted[lol-1] = sorted[lol-1],sorted[lol]
+                            lol -= 1
+                del matrix[tmp]
+                del weights[tmp]
+            for i in range(1,len(sorted)):
+                if sorted[i] > sorted[0]: sorted[i] -= 1
+            del sorted[0]
+        
+        while len(pairs) > len(matrix)+10:
+            while weights[sorted[0]] == 0:
+                del matrix[sorted[0]]
+                del weights[sorted[0]]
+                for k in range(1,len(sorted)):
+                    if sorted[k] > sorted[0]: sorted[k] -= 1
+                del sorted[0]
+            need = True
+            line = sorted[0]
+            coeff = matrix[line][0]
+            del pairs[coeff]
+            i = 0
+            while i < len(matrix):
+                stored = -1
+                for j in range(weights[i]):
+                    if matrix[i][j] == coeff: stored = j
+                    elif matrix[i][j] > coeff: matrix[i][j] -= 1
+                if stored > -1:
+                    del matrix[i][stored]
+                    weights[i] -= 1
+                    if not weights[i]:
+                        del weights[i]
+                        del matrix[i]
+                        stored = -1
+                        for z in range(len(sorted)):
+                            if sorted[z] == i: stored = z
+                            elif sorted[z] > i: sorted[z] -= 1
+                        del sorted[stored]
+                    else:
+                        lol = sorted.index(i)
+                        while lol > 0 and weights[sorted[lol]] < weights[sorted[lol-1]]:
+                            sorted[lol],sorted[lol-1] = sorted[lol-1],sorted[lol]
+                            lol -= 1
+                        i += 1
+                else: i += 1
+                
+    return matrix
+                        
+# Build the dense binary matrix by computing explicitely every A[i][j]
+def build_dense_matrix(pairs,rat,alg,qua,div_lead):
+    matrix = []
+    for r in pairs:
+        line = []
+        
+        if r[3] < 0: line.append(1)
+        else: line.append(0)
+
+        for p in rat:
+            tmp = p
+            tmp2 = 0
+            while not r[3]%tmp:
+                tmp *= p
+                tmp2 ^= 1
+            line.append(tmp2)
+            
+        if r[1] < 0: line.append(1)
+        else: line.append(0)
+
+        for p in range(len(alg)):
+            tmp = rat[p]
+            tmp2 = 0
+            while not r[1]%tmp:
+                tmp *= rat[p]
+                tmp2 ^= 1
+            for i in range(len(alg[p])):
+                if not eval_mod(r[5],alg[p][i],rat[p]):
+                    line.append(tmp2)
+                else:
+                    line.append(0)
+                
+        for p in range(len(qua)):
+            if compute_legendre_character(eval_mod(r[5],qua[p][1],qua[p][0]),qua[p][0]) == -1:
+                line.append(1)
+            else:
+                line.append(0)
+            
+        for p in range(len(div_lead)):
+            if r[7+p]:
+                tmp = div_lead[p]
+                tmp2 = 0
+                while not r[1]%tmp:
+                    tmp *= div_lead[p]
+                    tmp2 ^= 1
+                line.append(tmp2)
+            else: line.append(0)
+            
+        line.append(r[6]&1)
+
+        matrix.append(line)
+    return matrix
+    
+def siqs_build_matrix_opt(M):
+    """Convert the given matrix M of 0s and 1s into a list of numbers m
+    that correspond to the columns of the matrix.
+    The j-th number encodes the j-th column of matrix M in binary:
+    The i-th bit of m[i] is equal to M[i][j].
+    """
+    m = len(M[0])
+    cols_binary = [""] * m
+    for mi in M:
+        for j, mij in enumerate(mi):
+            cols_binary[j] += "1" if mij else "0"
+    return [int(cols_bin[::-1], 2) for cols_bin in cols_binary], len(M), m
